9f045c7664f35e18caf4ac524d277ea40d12535f
Ahmed Khattab (ahkhatta) <ahkhatta@cisco.com>
2017-04-15 13:18:36 -0700

    added a second function for calculating square

--- src/my_square.py
+++ src/my_square.py
@@ -1,4 +1,11 @@
 def my_square(x):
+	""" takes a value and returns the squared value.
+            uses the * operator
+        """
 	return (x ** 2)
 
-print (my_square(5))
+def my_square2(x):
+	return (x * x)
+
+print (my_square(42))
+print (my_square2(42))
